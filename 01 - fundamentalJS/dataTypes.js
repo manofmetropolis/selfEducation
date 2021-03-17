80753faf77ec8d2bd041f46e2654def49c76cac3
R Graff <rmgraff@gmail.com>
2021-03-17 12:36:05 -0500

    notes on js types

---
+++ 01 - fundamentalJS/dataTypes.js
@@ -0,0 +1,13 @@
+let number = 8
+let boolean = true
+let string = 'Mariah Carey is delightful'
+
+// null is the absence oof a value
+// bigInt is created by sticking an n at the end of a number to woork with numbers larger than the maximum "safe" integer for the number type.
+// undefined is the absence of a value, but it's different
+// symbol is a unique identifier for more complex coding
+
+console.log(number + ' is a ' + typeof number);
+console.log('True and false have a type called ' + typeof boolean);
+console.log(string + ' is an example of a ' + typeof string);
+
