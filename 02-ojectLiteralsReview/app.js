47477e7f1fba0a99db382c780c9f63108a8e9146
R Graff <rmgraff@gmail.com>
2021-03-23 11:51:11 -0500

    reorg

---
+++ 02-ojectLiteralsReview/app.js
@@ -0,0 +1,9 @@
+//Objects are collections of properties (which is a key-value pair). Rather than using an index to access data, we use them custom keys. Let's do one.
+
+const fitBitData = {
+    totalSteps: 308000,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+};
